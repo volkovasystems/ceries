1878d6fc3a7cf15ff2fb7677381131f58121f0d2
johnlenon <johnlenonmaghanoy@gmail.com>
2017-03-31 12:21:30 +0800

    Updated dependency.

---
+++ .install.sh
@@ -0,0 +1,34 @@
+yarn add \
+babel-runtime \
+budge \
+called \
+clazof \
+harden \
+letgo \
+plough \
+raze \
+snapd \
+vound \
+zelf \
+--save && \
+yarn add \
+babel-cli \
+babel-core \
+babel-plugin-transform-runtime \
+babel-preset-latest \
+source-map-loader \
+webpack \
+--dev && \
+bower install \
+budge \
+called \
+clazof \
+harden \
+letgo \
+plough \
+raze \
+snapd \
+vound \
+zelf \
+--save \
+--force-latest
