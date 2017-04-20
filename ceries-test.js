396173a8bf571d7a7584a9337c0c31b2253c2eec
johnlenon <johnlenonmaghanoy@gmail.com>
2017-04-20 12:00:19 +0800

    Updated dependency.

--- ceries-test.js
+++ ceries-test.js
@@ -1,3 +1,23 @@
 const ceries = require( "./ceries.js" );
 
-console.log( ceries( ) );
+ceries( [
+			function one( callback ) {
+			    console.log( "step 1" );
+			    callback( null, 23 );
+			},
+			function two( callback, option ) {
+			    console.log( "step 2", callback );
+			    callback( null, 24 );
+			},
+			function three( callback, option ) {
+			    console.log( "step 3", callback );
+			    callback( null, 25 );
+			}
+        ] )
+		( function lastly( error, option ) {
+			if( error ){
+			    console.log( "Error", error );
+			}else{
+			    console.log( "Option", option );
+			}
+		} );
